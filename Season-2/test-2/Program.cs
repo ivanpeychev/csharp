c634d24499704e793d5e86347bbef3e4a58fcdef
Ivan Kyuchukov <vaka.stz@gmail.com>
2018-10-29 20:40:46 +0200

    a

---
+++ Season-2/test-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string inputString = "Robot am I";
+
+			Console.WriteLine(
+				string.Join(" ", inputString
+					.Split(' ')
+					.Reverse())
+			);
+		}
+	}
+}
