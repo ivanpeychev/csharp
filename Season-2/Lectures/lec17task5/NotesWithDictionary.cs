3c0e5098ef62d04f0fdc4f119cd488c3df1dfa0d
Ivan Kyuchukov <vaka.stz@gmail.com>
2018-10-31 22:57:36 +0200

    a

---
+++ Season-2/Lectures/lec17task5/NotesWithDictionary.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class NotesWithDictionary
+{
+	static void Main()
+	{
+		var notes = new Dictionary<double, string>() 
+		{
+			{ 261.63, "C"  },
+			{ 277.18, "C#" },
+			{ 293.66, "D"  },
+			{ 311.13, "D#" },
+			{ 329.63, "E"  },
+			{ 349.23, "F"  },
+			{ 369.99, "F#" },
+			{ 392.00, "G"  },
+			{ 415.30, "G#" },
+			{ 440.00, "A"  },
+			{ 466.00, "A#" },
+			{ 493.88, "B"  }
+		};
+
+		var inputFreqs = Console.ReadLine()
+			.Split(' ')
+			.Select(double.Parse)
+			.ToList();
+
+		var naturals = new Dictionary<double, string>();
+		var sharps = new Dictionary<double, string>();
+		var outputNotes = new Dictionary<double, string>();
+
+		foreach (var inputItem in inputFreqs)
+		{
+			if(notes.TryGetValue(inputItem, out string value))
+			{
+				if (value.Contains("#"))
+				{
+					sharps.Add(inputItem, value);
+				}
+				else
+				{
+					naturals.Add(inputItem, value);
+				}
+				outputNotes.Add(inputItem, value);
+			}
+		}
+
+		if (outputNotes.Count > 0)
+		{
+			Console.WriteLine($"Notes: {string.Join(" ", outputNotes.Values)}");
+			Console.WriteLine($"Naturals: {string.Join(" ", naturals.Values)}");
+			Console.WriteLine($"Sharps: {string.Join(" ", sharps.Values)}");
+			Console.WriteLine($"Naturals' sum: {naturals.Keys.Sum()}");
+			Console.WriteLine($"Sharps' sum: {sharps.Keys.Sum()}");
+		}
+	}
+}
---
+++ Season-2/NotesWithList/lec17t5NoteStatistics.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class lec17t5NoteStatistics
+{
+	static void Main()
+	{
+		var notes = new Dictionary<double, string>()
+		{
+			{ 261.63, "C"  },
+			{ 277.18, "C#" },
+			{ 293.66, "D"  },
+			{ 311.13, "D#" },
+			{ 329.63, "E"  },
+			{ 349.23, "F"  },
+			{ 369.99, "F#" },
+			{ 392.00, "G"  },
+			{ 415.30, "G#" },
+			{ 440.00, "A"  },
+			{ 466.16, "A#" },
+			{ 493.88, "B"  }
+		};
+
+		var inputFreqs = Console.ReadLine()
+			.Split(' ')
+			.Select(double.Parse)
+			.ToList();
+
+		var naturals = new List<KeyValuePair<double, string>>();
+		var sharps = new List<KeyValuePair<double, string>>();
+		var allNotes = new List<KeyValuePair<double, string>>();
+
+		foreach (double key in inputFreqs)
+		{
+			if (notes.TryGetValue(key, out string value))
+			{
+				if (value.Contains("#"))
+				{
+					sharps.Add(new KeyValuePair<double, string>(key, value));
+				}
+				else
+				{
+					naturals.Add(new KeyValuePair<double, string>(key, value));
+				}
+				allNotes.Add(new KeyValuePair<double, string>(key, value));
+			}
+		}
+
+		if (allNotes.Count > 0)
+		{
+			Console.WriteLine($"Notes: {string.Join(" ", allNotes.Select(i => i.Value))}");
+			Console.WriteLine($"Naturals: {string.Join(", ", naturals.Select(i => i.Value))}");
+			Console.WriteLine($"Sharps: {string.Join(", ", sharps.Select(i => i.Value))}");
+			Console.WriteLine($"Naturals sum: {naturals.Select(i => i.Key).Sum()}");
+			Console.WriteLine($"Sharps sum: {sharps.Select(i => i.Key).Sum()}");
+		}
+
+		//allNotes.ForEach(i => Console.Beep((int)i.Key, 300));
+	}
+}
