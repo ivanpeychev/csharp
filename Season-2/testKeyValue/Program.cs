60344a9de922bdf8d03bd0c2667d75e1af13029d
Ivan Kyuchukov <vaka.stz@gmail.com>
2018-11-13 07:33:05 +0000

    a

---
+++ Season-2/testKeyValue/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace testKeyValue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myList = new List<KeyValuePair<string, string>>();
+
+            myList.Add(new KeyValuePair<string, string>("hi", "bye"));
+
+            Console.WriteLine(myList[0]);
+        }
+    }
+}
