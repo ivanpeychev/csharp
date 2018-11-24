03fd42c1ccc3498084f642d232d8ece100dfd5ec
Ivan Kyuchukov <vaka.stz@gmail.com>
2018-11-24 16:04:22 +0200

    a

---
+++ Season-2/L25T2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L25T2
+{
+	class Program
+	{
+		static void Main()
+		{
+			var myList = "10 30 15 20 50 5"
+				.Split(' ')
+				.Select(int.Parse)
+				.ToList();
+
+			myList.Sort();
+			myList.Reverse();
+
+			Console.WriteLine(string.Join(" ", myList.Take(3)));
+		}
+	}
+}
---
+++ Season-2/L25T3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+	static void Main()
+	{
+		var myList = "In SoftUni you can study Java, C#, PHP and JavaScript. JAVA and c# developers graduate in 2-3 years. Go in!"
+			.Split(new char[] { '.', ',', ':', ';', '(', ')', '[', ']', '&', '"', '\'', '\\', '/', '!', '?', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+			.ToList();
+
+		var resultList = new List<string>();
+
+		foreach (var word in myList)
+		{
+			var currentWord = word.ToLower();
+			if (resultList.Contains(word.ToLower()))
+			{
+				
+			}
+		}
+	}
+}
