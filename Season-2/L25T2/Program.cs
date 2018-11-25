59b949d331b575497f135318492d225164b047ba
Ivan Kyuchukov <vaka.stz@gmail.com>
2018-11-25 13:07:21 +0000

    a

---
+++ Season-2/L25T2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L25T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myList = "10 30 15 20 50 5"
+                .Split(' ')
+                .OrderByDescending(i => i)
+                .ToList();
+
+            myList.ForEach(Console.WriteLine);
+        }
+    }
+}
