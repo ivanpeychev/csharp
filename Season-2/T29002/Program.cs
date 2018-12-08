12111eb6c39cac4d1d84da56c2763eb40788924c
Ivan Kyuchukov <vaka.stz@gmail.com>
2018-12-08 05:36:52 +0200

    k

---
+++ Season-2/T29002/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+class Program
+{
+	static void Main()
+	{
+		string basePath = @"D:\Desktop\Temp";
+
+		var files = Directory.GetFiles(basePath);
+
+		foreach (var file in files)
+		{
+			string fileName = Path.GetFileName(file);
+			string extension = Path.GetExtension(fileName);
+
+			string subDir = string.Format($@"{basePath}\{extension.TrimStart('.')}");
+			if (!Directory.Exists(subDir))
+			{
+				Directory.CreateDirectory(subDir);
+			}
+
+			string newFile = string.Format($@"{subDir}\{fileName}");
+			File.Move(file, newFile);
+		}
+	}
+}
