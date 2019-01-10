d7f203b37eabcc1c5fa4e6d2a85a976430459596
Ivan Kyuchukov <vaka.stz@gmail.com>
2019-01-10 09:29:32 +0000

    h

---
+++ Season-2/AsyncT0_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        PrintNumbersInRange(0, 10);
+
+        var task = Task.Run(() =>
+            PrintNumbersInRange(10, 20));
+
+        Console.WriteLine("Done");
+        task.Wait();
+    }
+
+    private static void PrintNumbersInRange(int v1, int v2)
+    {
+        for (int i = v1; i < v2; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
