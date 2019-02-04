6f8090c28d23b3fcd968f891e8ede1c0992ec3fa
Ivan Kyuchukov <vaka.stz@gmail.com>
2019-02-04 20:04:06 +0200

    Adding new solution to csharp

---
+++ Season-2/L35L7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+class Program
+{
+	static void Main()
+	{
+		var pattern = "<a\\s*href=\"(.*?)\">(.*?)<\\/a>";
+		var text = "<ul>   <li>      <a href=\"http://softuni.bg\">SoftUni</a>  <li/>  </ul> <ul>   <li>      <a href=\"http://google.com\">Google</a>  <li/>  </ul>";
+		var regEx = new Regex(pattern);
+		var replacement = "[URL href=\"$1\">$2[/URL]";
+
+		foreach (Match match in regEx.Matches(text))
+		{
+			Console.WriteLine(match.Groups[1]);
+			Console.WriteLine(match.Groups[2]);
+			Console.WriteLine();
+		}
+
+		var replaced = regEx.Replace(text, replacement);
+		Console.WriteLine(replaced);
+	}
+}
---
+++ Season-2/L37T1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L37T1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Time of departure:");
+			DateTime departureTime = DateTime.Parse(Console.ReadLine());
+
+			Console.WriteLine("Steps to home:");
+			int stepsToHome = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Seconds per step:");
+			int secPerStep = int.Parse(Console.ReadLine());
+
+			Console.WriteLine(departureTime.AddSeconds(stepsToHome * secPerStep));
+		}
+	}
+}
