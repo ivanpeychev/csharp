dc4f32d95fe0348becbc8831dc0fe1d503923ac9
Ivan Kyuchukov <vaka.stz@gmail.com>
2019-02-06 08:26:17 +0000

    fa

---
+++ CSharpWeb/L4_T1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+
+class Program
+{
+    static void Main()
+    {
+        string input = "http://url-decoder.com/i%23de%25?id=23";
+        string decodedUrl = WebUtility.UrlDecode(input);
+
+        Console.WriteLine(decodedUrl);
+    }
+}
---
+++ CSharpWeb/L4_T2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+
+namespace L4_T2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "https/mysite.bg?id=2";
+            string decodedUrl = WebUtility.UrlDecode(input);
+
+            bool isValidUrl = Uri.TryCreate(decodedUrl, UriKind.Absolute, out Uri uri);
+
+            if (isValidUrl)
+            {
+                Console.Write("Is Valid: ");
+                Console.WriteLine(isValidUrl);
+                Console.Write("Protocol: ");
+                Console.WriteLine(uri.Scheme);
+                Console.Write("Host: ");
+                Console.WriteLine(uri.Host);
+                Console.Write("Port: ");
+                Console.WriteLine(uri.Port);
+                Console.Write("Path: ");
+                Console.WriteLine(uri.LocalPath);
+                Console.Write("Query: ");
+                Console.WriteLine(uri.Query);
+                Console.Write("Fragment: ");
+                Console.WriteLine(uri.Fragment);
+            }
+            else
+            {
+                Console.WriteLine("Invalid URL!");
+            }
+        }
+    }
+}
